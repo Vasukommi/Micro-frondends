ef0eaf97f7d65182b2f528f5f824abcfb2dcaa58
vasu kommi <itsvasukommi@gmail.com>
2023-03-03 02:49:26 +0530

    initial commit

---
+++ child-vue/src/component/info.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="card">
+        Child: Vue Component
+</div>
+</template>
+
+<style scoped>
+.card {
+    border: 1px solid #085e00;
+    background-color: #22e965;
+    text-align: center;
+    padding: 20px 0px;
+    color: black;
+}
+</style>
